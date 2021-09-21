456b6026e81d8f27b534c1452f170dfd9f70cf63
Saikat Bishal <saikat.bishal786@gmail.com>
2021-09-21 15:22:14 +0530

    On a train journey for more than 24 hours.

---
+++ src/com/saikat/PrintMessage.java
@@ -0,0 +1,7 @@
+package com.saikat;
+
+public class PrintMessage {
+    public static void main(String[] args) {
+        System.out.println("Today I'm travelling from Kharagpur to Katpadi for a yearly medical check-up.\nI'd be on Train the full day.");
+    }
+}
