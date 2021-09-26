11e60bee9e1445bf486fe00e2ba1d6ebf8553a73
Saikat Bishal <saikat.bishal786@gmail.com>
2021-09-26 21:42:38 +0530

    precision of square root (not fully working)

--- src/com/saikat/Sqrt69.java
+++ src/com/saikat/Sqrt69.java
@@ -7,10 +7,22 @@ public class Sqrt69 {
         Scanner in = new Scanner(System.in);
         System.out.println("Input a number to find its square root");
         int n = in.nextInt();
-        System.out.println(sqrt(n));
+        //System.out.println(sqrt(n));
+        System.out.println(preciseSqrt(n));
 
     }
-    static int sqrt(int x)
+    static double preciseSqrt(int n)
+    {
+
+        double x1 = sqrt(n);
+        System.out.println(x1);
+        while(x1*x1<n)
+        {
+            x1+=0.1;
+        }
+        return x1;
+    }
+    static double sqrt(int x)
     {
         long low=0L;
         long high = x/2+1L;
